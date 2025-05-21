83a3770f4d3fad5ac5f7701bec878c900075fee7
True Cookie <artemogloblin56@gmail.com>
2025-05-21 19:24:44 +0300

    Рабочий прототип (файл comment.py)

--- comment.py
+++ comment.py
@@ -3,16 +3,18 @@ from playwright.sync_api import sync_playwright
 import os
 
 COMMUNITY_NAME = "asyabio"
-
-
+        
 
 playwright = sync_playwright().start()
 browser = playwright.chromium.connect_over_cdp("http://localhost:9222")
 # browser = playwright.chromium.launch(headless=False)
-page = browser.new_page()
+
+# Получить первую открытую страницу
+page = browser.contexts[0].pages[0]
+# page = browser.new_page()
 
 # Открываем страницу сообщества ВКонтакте
-page.goto(f'https://vk.com/{COMMUNITY_NAME}')  # Замените на имя вашего сообщества
+# page.goto(f'https://vk.com/{COMMUNITY_NAME}')  # Замените на имя вашего сообщества
 
 # Ждем, пока страница загрузится
 page.wait_for_load_state('networkidle')
@@ -22,9 +24,11 @@ posts =  page.query_selector_all("//div[contains(@class, 'post') and starts-with
 if posts:
     latest_post = posts[0]
 
-    comment_button =  latest_post.query_selector("//div[contains(@class, 'post') and starts-with(@id, 'post-216822404_')]//div[contains(@class, 'comment')]")
+    comment_button = latest_post.query_selector("//div[contains(@class, 'comment')]")
     comment_button.click()
 
     # Вводим текст комментария
-    page.fill('textarea', 'Меркулова Анастасия')  # Замените селектор на актуальный для текстового поля комментария
-    page.click('button[type="submit"]')  # Замените селектор на актуальный для кнопки отправки комментария
+    page.fill("//div[contains(@class, 'post') and starts-with(@id, 'post-216822404_')]//div[contains(@class, 'reply_field') and contains(@class, 'submit_post_field')]", 'Меркулова Анастасия, 301 пед')  # Замените селектор на актуальный для текстового поля комментария
+    
+    page.wait_for_selector("//button[contains(@class, 'reply_send_button')]", timeout=5000)
+    page.click("//button[contains(@class, 'reply_send_button')]")  # Замените селектор на актуальный для кнопки отправки комментария
